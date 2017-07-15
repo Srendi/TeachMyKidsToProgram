f57aa7e17c4f4c6e9f9006cb04e86230ccb2de3c
Gavin Ellis <srendi@gmail.com>
2017-07-15 17:57:30 +0800

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,24 @@
+# Author: Lily and Dad
+# Date: 15 July 2017
+# Description: A simple arithmatic calculator
+# Todo: Add more operators
+
+def main():
+	varA = input("Please enter a number: ")
+	varB = input("Please enter a another number: ")
+	varOperator = raw_input("please enter an operator (+,-,*,/): ")
+	print("You entered " + str(varOperator))
+	if str(varOperator) == "+":
+		resultNum = str(varA+varB)
+	elif str(varOperator) == "*":
+		resultNum = str(varA*varB)
+	elif str(varOperator) == "-":
+		resultNum = str(varA-varB)
+	elif str(varOperator) == "/":
+		resultNum = str(varA/varB)
+	else:
+		print("Unknown operator try plus, minus, multiplication, or division")
+	print(str(varA) + " " + str(varOperator) + " " + str(varB) + " = " + str(resultNum))
+
+if __name__ == "__main__":
+	main()
